cccd03039036978faf8b80d11a3c1f7b120a8586
Boris Gordon <boris@boztek.net>
2017-04-20 15:26:23 +1000

    Added a behaviour

--- src/ui/components/glimmer-deploy-test/component.ts
+++ src/ui/components/glimmer-deploy-test/component.ts
@@ -1,5 +1,18 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class GlimmerDeployTest extends Component {
-    title: string = "This is a glimmer component";
+    title: string = "This is a top level component";
+
+    @tracked
+    state: string = "ENTER VALUE ...";
+
+    passTextToSubComp(e: Event): void {
+        let text: string = this.getText(e);
+        this.state = text;
+    }
+
+    getText(e: Event): string {
+        let el: HTMLInputElement = e.target as any;
+        return el.value;
+    }
 }
